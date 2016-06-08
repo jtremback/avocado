2e1c5aca36818e143e2c43fa6a7be68151f726ae
ameen <ameensol@gmail.com>
2016-06-08 14:46:41 -0700

    use root level ethereum setup file for both offchain tests and unit tests

--- offchain/test/setup.js
+++ offchain/test/setup.js
@@ -1,53 +1,9 @@
+import setup from '../../setup'
 import { Logic } from '../logic.js'
-import fs from 'fs'
-import Web3 from 'web3'
-import Pudding from 'ether-pudding'
-import p from 'es6-promisify'
-import TestRPC from 'ethereumjs-testrpc'
-import solc from 'solc'
 
-const SOL_PATH = __dirname + '/../../contracts/'
-const PUDDING_PATH = __dirname + '/../../pudding/'
-const TESTRPC_PORT = 8545
+export default async () => {
 
-export default async function () {
-  // COMPILE AND PUDDINGIFY
-  // Everything in this section could be moved into another process
-  // watching ./contracts
-  const input = {
-    'ECVerify.sol': fs.readFileSync(SOL_PATH + 'ECVerify.sol').toString(),
-    'StateChannels.sol': fs.readFileSync(SOL_PATH + 'StateChannels.sol').toString()
-  }
-
-  const output = solc.compile({ sources: input }, 1)
-  if (output.errors) { throw new Error(output.errors) }
-
-  await Pudding.save({
-    abi: JSON.parse(output.contracts['StateChannels'].interface),
-    binary: output.contracts['StateChannels'].bytecode
-  }, PUDDING_PATH + './StateChannels.sol.js')
-
-
-  // START TESTRPC
-  await p(TestRPC.server({
-    mnemonic: 'elegant ability lawn fiscal fossil general swarm trap bind require exchange ostrich'
-  }).listen)(TESTRPC_PORT)
-
-
-
-  // MAKE WEB3
-  const web3 = new Web3()
-  web3.setProvider(new Web3.providers.HttpProvider('http://localhost:' + TESTRPC_PORT))
-  const accounts = await p(web3.eth.getAccounts)()
-  web3.eth.defaultAccount = accounts[0]
-
-
-  // INSTANTIATE PUDDING CONTRACT ABSTRACTION
-  const StateChannels = require(PUDDING_PATH + './StateChannels.sol.js')
-  StateChannels.setProvider(new Web3.providers.HttpProvider('http://localhost:' + TESTRPC_PORT))
-  const contract = await StateChannels.new()
-
-
+  let { web3, contract, accounts } = await setup()
 
   // SET UP AND RUN TESTS
   const aliceFakeStore = {}
--- test/stateChannels.js
+++ test/stateChannels.js
@@ -1,83 +1,11 @@
-// TODO: refactor away from truffle format
-
-/*global StateChannels web3 Uint8Array*/
 import test from 'blue-tape'
-import sha3 from 'js-sha3'
 import leftPad from 'left-pad'
 import p from 'es6-promisify'
-import fs from 'fs'
-import Web3 from 'web3'
-import Pudding from 'ether-pudding'
-import TestRPC from 'ethereumjs-testrpc'
-import solc from 'solc'
+import setup from '../setup'
 
-const keccak = sha3.keccak_256
+test('StateChannels', async () => {
 
-const SOL_PATH = __dirname + '/../contracts/'
-const PUDDING_PATH = __dirname + '/../pudding/'
-const TESTRPC_PORT = 8545
-
-let web3
-
-function solSha3 (...args) {
-    args = args.map(arg => {
-        if (typeof arg === 'string') {
-            if (arg.substring(0, 2) === '0x') {
-                return arg.slice(2)
-            } else {
-                return web3.toHex(arg).slice(2)
-            }
-        }
-
-        if (typeof arg === 'number') {
-            return leftPad((arg).toString(16), 64, 0)
-        }
-    })
-
-    args = args.join('')
-
-    return '0x' + web3.sha3(args, { encoding: 'hex' })
-}
-
-async function setup() {
-  // COMPILE AND PUDDINGIFY
-  // Everything in this section could be moved into another process
-  // watching ./contracts
-  const input = {
-    'ECVerify.sol': fs.readFileSync(SOL_PATH + 'ECVerify.sol').toString(),
-    'StateChannels.sol': fs.readFileSync(SOL_PATH + 'StateChannels.sol').toString()
-  }
-
-  const output = solc.compile({ sources: input }, 1)
-  if (output.errors) { throw new Error(output.errors) }
-
-  await Pudding.save({
-    abi: JSON.parse(output.contracts['StateChannels'].interface),
-    binary: output.contracts['StateChannels'].bytecode
-  }, PUDDING_PATH + './StateChannels.sol.js')
-
-  // START TESTRPC
-  await p(TestRPC.server({
-    mnemonic: 'elegant ability lawn fiscal fossil general swarm trap bind require exchange ostrich'
-  }).listen)(TESTRPC_PORT)
-
-  // MAKE WEB3
-  web3 = new Web3()
-  web3.setProvider(new Web3.providers.HttpProvider('http://localhost:' + TESTRPC_PORT))
-  const accounts = await p(web3.eth.getAccounts)()
-  web3.eth.defaultAccount = accounts[0]
-
-  // INSTANTIATE PUDDING CONTRACT ABSTRACTION
-  const StateChannels = require(PUDDING_PATH + './StateChannels.sol.js')
-  StateChannels.setProvider(new Web3.providers.HttpProvider('http://localhost:' + TESTRPC_PORT))
-
-  const contract = await StateChannels.new()
-
-  return { contract, accounts }
-}
-
-test('StateChannels', async t => {
-    const { contract, accounts } = await setup()
+    let { web3, contract, accounts } = await setup()
 
     test('adds channel and checks state', async t => {
         const challengePeriod = 1
@@ -287,67 +215,24 @@ test('StateChannels', async t => {
         t.end()
         process.exit(0)
     })
+
+    function solSha3 (...args) {
+        args = args.map(arg => {
+            if (typeof arg === 'string') {
+                if (arg.substring(0, 2) === '0x') {
+                    return arg.slice(2)
+                } else {
+                    return web3.toHex(arg).slice(2)
+                }
+            }
+
+            if (typeof arg === 'number') {
+                return leftPad((arg).toString(16), 64, 0)
+            }
+        })
+
+        args = args.join('')
+
+        return '0x' + web3.sha3(args, { encoding: 'hex' })
+    }
 })
-
-function byteToHexString(uint8arr) {
-    if (!uint8arr) {
-        return '';
-    }
-
-    var hexStr = '';
-    for (var i = 0; i < uint8arr.length; i++) {
-        var hex = (uint8arr[i] & 0xff).toString(16);
-        hex = (hex.length === 1) ? '0' + hex : hex;
-        hexStr += hex;
-    }
-
-    return hexStr.toUpperCase();
-}
-
-function hexStringToByte(str) {
-    if (!str) {
-        return new Uint8Array();
-    }
-
-    var a = [];
-    for (var i = 0, len = str.length; i < len; i += 2) {
-        a.push(parseInt(str.substr(i, 2), 16));
-    }
-
-    return new Uint8Array(a);
-}
-
-function concatenate(resultConstructor, ...arrays) {
-    let totalLength = 0;
-    for (let arr of arrays) {
-        totalLength += arr.length;
-    }
-    let result = new resultConstructor(totalLength);
-    let offset = 0;
-    for (let arr of arrays) {
-        result.set(arr, offset);
-        offset += arr.length;
-    }
-    return result;
-}
-
-// Extra Addresses
-
-// 0xF18506eD9AdcA974c0e803859994d11fc8753885
-// 579ac7f421b256fd8b9bd7a5f384f6499b98c9409f9d431137b9d69db129d65f
-
-// 0xF8D07F73f5336b8b77D52143906a216E454E8f3a
-// 14475f8d92fbee4e20ce9cb8fe8b434e57e5d80787e9ddd433ee66f848210ea9
-
-// 0x8fb411A5Bb2F0fa6B247409F05494B56E9Fa730a
-// 16add8e48cdfd12a07dd8ec86db7c284a41fbc0d7454272a332520bd2cf64180
-
-// 0xff7FC071Eb3385D1A810bAABD3d870156a965b12
-// 1d9cc52f5a6dbabb5dce7bec96fe729ca45a72323379d48b5641db36d5240c5d
-
-// 0xf8c138b08cb32391C7Ab8Edbda61E023943f72d7
-// 6712eb15afa15159ca2f8ae405bb6286929e81b1d1865186717500202cfcf9b8
-
-// 0x763e646f269d9c50f24d2c4802859ccd185148497774bff4525426d4eb771d0b23e5157cc8dba35bd6eb075cbe7e3854e2775ad44f8c5ae3d3c7ec7c278947081b
-
-// 41b1a0649752af1b28b3dc29a1556eee781e4a4c3a1f7f53f90fa834de098c4d
